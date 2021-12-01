4a695629606fa1fcf167b6a73a2142739244756d
leonavas <rodrigoleonavas@gmail.com>
2021-11-30 22:27:38 -0300

    vscode theme for prism, svelte highlight for prism and copy code button

--- src/lib/demo/CodeBlock.svelte
+++ src/lib/demo/CodeBlock.svelte
@@ -1,36 +1,60 @@
 <script>
 	import { onMount } from 'svelte';
-    import Prism from 'prismjs';
+	import Prism from 'prismjs';
+	import 'prism-svelte';
+	export let lang = 'js';
+	import { fade } from 'svelte/transition';
 
 	onMount(() => {
-		// let script = document.createElement('script');
-		// script.src = './prism.js';
-		// document.head.append(script);
-        Prism.highlightAll();
+		Prism.highlightAll();
 	});
 
-	// setTimeout(() => {
-	// 	window.Prism = window.Prism || {};
-	// 	console.log(window.Prism);
-	// }, 2000);
+	export let code = '';
 
-	let code = `# Create a function to print squares of numbers in sequence.
-new.function <- function(a) {
-   for(i in 1:a) {
-      b <- i^2
-      print(b)
-   }
-}
-
-# Call the function new.function supplying 6 as an argument.
-new.function(6)
-`;
+	let copied = false;
 </script>
 
+<div class="relative group">
 
-<pre>
-	<code class="language-js">{code}</code>
-</pre>
-<!-- <svelte:head>
-  <link rel="stylesheet" href="https://tutsplus.github.io/syntax-highlighter-demos/highlighters/Prism/prism_okaidia.css">    
-</svelte:head> -->
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="absolute top-1 right-2 h-6 w-6 hover:text-gray-50 cursor-pointer text-gray-400"
+		fill="none"
+		on:click={() => {
+			navigator.clipboard.writeText(code);
+			copied = true;
+			setTimeout(() => {
+				copied = false;
+			}, 10);
+		}}
+		viewBox="0 0 24 24"
+		stroke="currentColor">
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+			d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0
+			002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
+	</svg>
+
+	{#if copied}
+		<svg
+			out:fade={{ delay: 2000, duration: 600 }}
+			xmlns="http://www.w3.org/2000/svg"
+			class="absolute top-1 right-2 h-6 w-6 text-emerald-400 cursor-pointer"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor">
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2
+				0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+		</svg>
+	{/if}
+
+	<pre class="!pt-7">
+		<code class={`language-${lang}`}>{code}</code>
+	</pre>
+</div>
--- src/routes/components/collection.svelte
+++ src/routes/components/collection.svelte
@@ -1,8 +1,9 @@
 <script>
 	import Collection from '$lib/Collection.svelte';
+	import CodeBlock from '$lib/demo/CodeBlock.svelte';
 </script>
 
-<Collection maxWait="10" log={true} path={'posts'} let:data let:last let:error>
+<Collection log={true} path={'posts'} let:data let:last let:error>
 
 	{#each data as post}{post.title}{/each}
 	<br />
@@ -11,3 +12,16 @@
 	<div slot="fallback">{error}</div>
 
 </Collection>
+
+<CodeBlock
+	lang="svelte"
+	code={`<Collection maxWait="10" log={true} path={'posts'} let:data let:last let:error>
+
+	{#each data as post}{post.title}{/each}
+	<br />
+	{last.title}
+	<div slot="loading">Carregando...</div>
+	<div slot="fallback">{error}</div>
+
+</Collection>
+`} />
